c60bf7096ace38a52fefe648bfd71746f92affd5
wktadmin <musicvae@gmail.com>
2018-07-14 23:16:04 +0800

    限制频率

--- apps/ext.py
+++ apps/ext.py
@@ -1,12 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
-#from flask_bootstrap import Bootstrap
+from flask_limiter import Limiter as _Limiter
+from flask_limiter.util import get_remote_address
+from apps.libs.error import ApiException
+from flask_limiter.errors import RateLimitExceeded
+
+
+class Limiter(_Limiter):
+
+    def __evaluate_limits(self, endpoint, limits):
+        try:
+            super(Limiter, self).__evaluate_limits(endpoint, limits)
+        except RateLimitExceeded:
+            raise ApiException(msg='访问速度过快, you\'re to fast', code=439, error_code=999)
+
 from flask_migrate import Migrate
-# from apps.views import create_blueprint
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["30/second", "100/day"],
+)
 db = SQLAlchemy()
 migrate = Migrate(db=db)
-#bootstrap = Bootstrap()
 
 def create_ext(app):
+    limiter.init_app(app)
     migrate.init_app(app) 
-    #bootstrap.init_app(app)
     db.init_app(app) 
--- apps/settings.py
+++ apps/settings.py
@@ -2,4 +2,4 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = ''
 SECRET_KEY = ''
-BASE_URL = 'http://wkt.ooo/'
+BASE_URL = ''
