944bf873e22966b19accd8fe043cfdb3efe38307
wktadmin <musicvae@gmail.com>
2018-07-31 20:50:31 +0800

    删除不必要的空行

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 from apps import create_app
-
 application = create_app()
-#manager = Manager(app)
-#manager.add_command('db', MigrateCommand)
-
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
     application.run()
-    #app.run()
+    # app.run()
